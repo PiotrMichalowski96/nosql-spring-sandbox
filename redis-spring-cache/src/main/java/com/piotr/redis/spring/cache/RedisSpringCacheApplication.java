760d428db98091c2b0277e2a380031a1e99a3dc1
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-05-12 18:15:56 +0200

    init

---
+++ redis-spring-cache/src/main/java/com/piotr/redis/spring/cache/RedisSpringCacheApplication.java
@@ -0,0 +1,13 @@
+package com.piotr.redis.spring.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringCacheApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RedisSpringCacheApplication.class, args);
+  }
+
+}

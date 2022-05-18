f65542f27c8924146519517a4e49af09d66712b5
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-05-18 18:23:25 +0200

    init

---
+++ redis-reactive-data/src/main/java/com/piotr/redis/reactive/data/RedisReactiveDataApplication.java
@@ -0,0 +1,13 @@
+package com.piotr.redis.reactive.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisReactiveDataApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RedisReactiveDataApplication.class, args);
+  }
+
+}

545ece996bb704b13b30449409cd10c18171d173
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-07-02 14:50:37 +0200

    Refactoring

--- redis-data/src/main/java/com/piotr/redis/data/service/StockService.java
+++ redis-data/src/main/java/com/piotr/redis/data/service/StockService.java
@@ -2,6 +2,7 @@ package com.piotr.redis.data.service;
 
 import com.piotr.redis.data.entity.Stock;
 import com.piotr.redis.data.repository.StockRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,17 +12,15 @@ public class StockService {
 
   private final StockRepository stockRepository;
 
-  public Stock getStockById(Long id) {
-    return stockRepository.findById(id)
-        .orElseThrow(RuntimeException::new);
+  public Optional<Stock> getStockById(Long id) {
+    return stockRepository.findById(id);
   }
 
   public Stock save(Stock stock) {
     return stockRepository.save(stock);
   }
 
-  public void delete(Long stockId) {
-    stockRepository.findById(stockId)
-        .ifPresent(stockRepository::delete);
+  public void delete(Stock stock) {
+    stockRepository.delete(stock);
   }
 }
